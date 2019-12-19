9e8356a795603c5512474e01a9381f80fa1d9bbe
Divyesh Chitroda <div5yesh@gmail.com>
2019-12-18 20:00:24 -0500

    visualizations and final dev results

--- NLP_Project.py
+++ NLP_Project.py
@@ -30,10 +30,6 @@ embeds = Embeddings()
 concepts = list(df[["Concept","Feature"]].groupby("Concept").groups.keys())
 features = list(df[["Concept","Feature"]].groupby("Concept")["Feature"].apply(list))
 
-dict_features = []
-for feat in features:
-    dict_features += [dict.fromkeys(feat, True)]
-
 #%%
 x = []
 for i in features:
@@ -41,8 +37,24 @@ for i in features:
     
 x = np.array(x)
 val, co = np.unique(x, return_counts=True)
-val[np.argsort(co)[-50:]]
-co[np.argsort(co)[-50:]]
+val_ = val[np.argsort(co)[-50:]]
+co_ = co[np.argsort(co)[-50:]]
+
+# %%
+plt.figure(figsize=(10, 5))
+plt.title("Top 50 hypernym distribution")
+plt.xlabel("Features")
+plt.ylabel("Frequency")
+plt.tight_layout()
+plt.xticks(rotation='vertical')
+plt.bar(np.flip(val_), np.flip(co_))
+
+#%%
+dict_features = []
+for feat in features:
+    dft = feat
+    # dft = list(filter(lambda x: x in val_, feat))
+    dict_features += [dict.fromkeys(dft, True)]
 
 # %%
 dv = DictVectorizer(sparse=False)
@@ -69,12 +81,13 @@ devY = Y[450:499]
 testY = Y[500:]
 
 # %%
+labels = 2526
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(12 * 768, 300)
-        self.fc2 = nn.Linear(300, 100)
-        self.fc3 = nn.Linear(100, 2526)
+        self.fc1 = nn.Linear(12 * 768, 1000)
+        self.fc2 = nn.Linear(1000, 300)
+        self.fc3 = nn.Linear(300, labels)
 
     def forward(self, x):
         x = x.view(-1, 12 * 768)
@@ -91,13 +104,14 @@ net.to(device)
 net.double()
 # torch.autograd.set_detect_anomaly(True)
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.2)
+# pos_weight = torch.ones(Y.shape[1]) * 5
 criterion = nn.BCELoss()
+# criterion.to(device)
 
 epochs = 100
 batch_size = 4
 run_loss = []
 batches = int(len(trainX)/batch_size)
-print(batches)
 for epoch in range(epochs):
     for batch_idx in range(batches):
         start = batch_size * batch_idx
@@ -109,13 +123,19 @@ for epoch in range(epochs):
 
         optimizer.zero_grad()
         net_out = net(data)
-        loss = criterion(net_out.reshape((-1,2526)), target.reshape((-1,2526)))
+        loss = criterion(net_out.reshape((-1,labels)), target.reshape((-1,labels)))
+        
+        # if epoch % 10 == 0:
+            # print('Before backward pass: \n', net[0].weight)
+            # s = torch.sum(net.fc3.weight.data)
         loss.backward()
         optimizer.step()
-        if batch_idx % batch_size == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, start , len(trainX),
-                           100. * batch_idx / len(trainX), loss.data))
+        # if (epoch+1) % 5 == 0:
+            # print('After backward pass: \n', net[0].weight)
+            # s = torch.sum(net.fc3.weight.data)
+
+        
+    print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, loss.data))
     run_loss.append(loss.item())
 
 # %%
@@ -129,6 +149,7 @@ test_loss = 0
 correct = 0
 precision = 0
 threshold = 0.99
+label_threshold = 0.1
 recall = 0
 
 with torch.no_grad():
@@ -139,10 +160,10 @@ with torch.no_grad():
     net_out = net(data)
     test_loss += criterion(net_out, target).data
     out = net_out.data
-    
+    x  = out.clone()
     # print(out.min(axis=1))
-    out[out > 0.8] = 1
-    out[out <= 0.8] = 0
+    out[out > label_threshold] = 1
+    out[out <= label_threshold] = 0
 
 for idx, act in enumerate(target):
     pred = out[idx]
@@ -165,5 +186,19 @@ for idx, act in enumerate(target):
 print("Accuracy:", correct/len(devX))
 print("Precision:", precision/len(devX))
 print("Recall:", recall/len(devX))
+
+# %%
+out = net(devX.cuda())
+
+#%%
+fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(10,10))
+idx = 0
+for row in ax:
+    for col in row:
+        col.scatter(out[idx].tolist(),range(2526))
+        col.plot([0.15,0.15],[0,2526], c="r")
+        idx += 1
+plt.tight_layout()
+plt.show()
 
 # %%
